b81e417c7c146061c65b0b2b0d5c091895ff07f8
John <jrc2855@gmail.com>
2023-03-30 01:14:26 +0000

    Authentication works, just doesn't render anything specific on screen

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,5 +1,7 @@
 import { createStyles, Group, Header, Navbar } from "@mantine/core";
+import { useContext, useState } from "react";
 import { Link } from 'react-router-dom';
+import { AuthContext } from "../../Context/Auth";
 
 const useStyles = createStyles((theme) => ({
   navbar: {
@@ -12,12 +14,37 @@ const useStyles = createStyles((theme) => ({
 
 const HeaderComponent = () => {
   const { classes } = useStyles();
+  const { login, logout, isLoggedIn } = useContext(AuthContext);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  console.log('isLoggedIn', isLoggedIn);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    login(username, password);
+    event.target.reset();
+  }
+
   return (
     <Header>
       <Navbar className={classes.navbar}>
         <Group>
           <Link className={classes.link} to="/" default>Home</Link>
           <Link className={classes.link} to="/settings">Settings</Link>
+        </Group>
+        <Group>
+          <form onSubmit={handleSubmit}>
+            <label>
+              username:
+              <input onChange ={(e) => setUsername(e.target.value)} />
+            </label>
+            <label>
+              password:
+              <input onChange = {(e) => setPassword(e.target.value)} />
+            </label>
+            <button type="submit">Login</button>
+          </form>
+          <button onClick={logout}>Logout</button>
         </Group>
       </Navbar>
     </Header>
--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import SettingsProvider from './Components/Todo/Context/Settings';
+import AuthProvider from './Context/Auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MantineProvider>
-      <SettingsProvider>
-        <App />
-      </SettingsProvider>
+      <AuthProvider>
+        <SettingsProvider>
+          <App />
+        </SettingsProvider>
+      </AuthProvider>
     </MantineProvider>
   </React.StrictMode>
 
